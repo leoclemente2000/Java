59d42e4714592f57ed9a0bea3930c02a6f89e3c1
leo <leonardoroderley@gmail.com>
2024-05-14 09:55:55 -0300

    Js professor

---
+++ tipo-numero.js
@@ -0,0 +1,14 @@
+// Tipo number
+
+// Declaracao variavel number
+
+const numero1 = 10;
+const numero2 = 10;
+
+const soma = numero1 + numero2;
+const subtracao = numero1 - numero2;
+
+// Mostra as operaçoes
+
+console.log(soma);
+console.log(subtracao)
